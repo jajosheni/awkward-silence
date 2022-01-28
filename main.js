0e0e1d3d2a405ba92e305b968b474113204701f0
Sheni Hamitaj <shen.i@live.com>
2022-01-28 21:25:16 +0100

    --add: infinite worker on every second to listen on tab

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
+const state = {
+  lastTime: new Date().getTime(),
+  now: () => new Date().getTime()
+};
 
-async function getCurrentTab() {
+async function getGoogleMeetTab() {
   let queryOptions = {url: 'https://meet.google.com/*'};
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab;
 }
 
-chrome.runtime.onInstalled.addListener(async () => {
-  setInterval(async () => {
-    const tab = await getCurrentTab();
+async function randomizeTopics() {
+  const tab = await getGoogleMeetTab();
 
-    if (tab?.audible) {
-      console.log('hoorayy');
+  console.log(state, tab?.audible);
+
+  if (!tab?.audible) {
+    if(state.now() - state.lastTime > 10 * 1000){
+      console.log('muted for 10 seconds, resetting timers');
+      state.lastTime = new Date().getTime();
     }
-  }, 1000);
+  } else {
+    state.lastTime = new Date().getTime();
+  }
+}
+
+chrome.runtime.onInstalled.addListener(async () => {
+  setInterval(randomizeTopics, 1000);
 });
